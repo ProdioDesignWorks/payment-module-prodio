b5a856c338edd78f7042b3b431cfa073d7433bc3
Shashikant Sharma <shashi@prodiodesignworks.in>
2018-11-21 13:57:51 +0530

    callback added

--- src/index.js
+++ src/index.js
@@ -2,21 +2,41 @@
 // eslint-disable-next-line import/prefer-default-export
 const axios = require('axios');
 const BASE_URL = `https://5m4hb8aet1.execute-api.us-west-2.amazonaws.com/prod/`;
+const HttpErrors = require('http-errors');
 
+const isNull = function (val) {
+  if (typeof val === 'string') { val = val.trim(); }
+  if (val === undefined || val === null || typeof val === 'undefined' || val === '' || val === 'undefined') {
+    return true;
+  }
+  return false;
+};
 
-function paymentServices(payload) {
-  this.execute = function (payload) {
+const isJson = function (str) {
+  try {
+    var obj = JSON.parse(JSON.stringify(str));
+    if (obj && typeof obj === 'object' && obj !== null) {
+      return true;
+    }
+  } catch (err) {
+    return false;
+  }
+  return false;
+}
+
+function paymentServices() {
+  this.execute = function (payload,callback) {
     // action key calls api.
     if(payload.action){
       switch(payload.action){
         case "CREATE_MERCHANT":
-          return funCreateMerchant(payload,BASE_URL);
+          return funCreateMerchant(payload,callback);
         break;
         case "GET_MERCHANT_ACTIVATION_STATUS":
-          return funMerchantActionvationStatus(payload,BASE_URL);
+          return funMerchantActionvationStatus(payload,callback);
         break;
         case "GET_MERCHANT_PROFILE":
-          return funMerchantProfile(payload,BASE_URL);
+          return funMerchantProfile(payload,callback);
         break;
         default:
           let errorMessage = `Please add BaseUrl.`;
@@ -32,13 +52,14 @@ function paymentServices(payload) {
 
 
 //creating user in  notification consumer model.
-const funCreateMerchant = function (payload, baseUrl,callback) {
+const funCreateMerchant = function (payload,callback) {
+  console.log("typeof =="+typeof callback);
   let userId = payload["meta"]["userId"];
   if (isNull(userId)) {
     return callback(new HttpErrors.BadRequest('User Id is mandatory.', { expose: false }));
   }
 
-  let url = `${baseUrl}ezpayMerchants/createMerchant?userId=${userId}`;
+  let url = `${BASE_URL}ezpayMerchants/createMerchant?userId=${userId}`;
   axios.post(url, payload).then(response => {
     return callback(response);
   }).catch((error) => {
@@ -47,12 +68,12 @@ const funCreateMerchant = function (payload, baseUrl,callback) {
   });
 }
 
-const funMerchantActionvationStatus = function (payload, baseUrl) {
+const funMerchantActionvationStatus = function (payload,callback) {
   let merchantId = payload["meta"]["merchantId"];
   if (isNull(merchantId)) {
     return callback(new HttpErrors.BadRequest('merchant Id is mandatory.', { expose: false }));
   }
-  let url = `${baseUrl}ezpayMerchants/getMerchantActivationStatus?merchantId=${merchantId}`;
+  let url = `${BASE_URL}ezpayMerchants/getMerchantActivationStatus?merchantId=${merchantId}`;
   axios.post(url, payload).then(response => {
     return callback(response);
   }).catch((error) => {
@@ -60,12 +81,12 @@ const funMerchantActionvationStatus = function (payload, baseUrl) {
   });
 }
 
-const funMerchantProfile = function (payload, baseUrl) {
+const funMerchantProfile = function (payload,callback) {
   let merchantId = payload["meta"]["merchantId"];
   if (isNull(merchantId)) {
     return callback(new HttpErrors.BadRequest('merchant Id is mandatory.', { expose: false }));
   }
-  let url = `${baseUrl}ezpayMerchants/getMerchantProfile?merchantId=${merchantId}`;
+  let url = `${BASE_URL}ezpayMerchants/getMerchantProfile?merchantId=${merchantId}`;
   axios.post(url, payload).then(response => {
     return callback(response);
   }).catch((error) => {
