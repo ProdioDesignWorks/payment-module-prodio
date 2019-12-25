5a977dadc34d713ffd87fd02d679a2b6ebde882a
Ankush Kolekar <ankush@prodio.in>
2019-12-25 17:33:36 +0530

    Get Site credentials list

--- src/index.js
+++ src/index.js
@@ -208,6 +208,9 @@ function paymentServices(BASE_URL) {
         case "LIST_SITES":
           return funListBusinessSites(BASE_URL, payload, callback);              
           break;
+        case "LIST_SITE_CREDENTIALS":
+          return funListSiteCredentials(BASE_URL, payload, callback);              
+          break;  
         case "SET_SITE_DEFAULT_CREDENTIALS":
           return funSetDefaultCredentials(BASE_URL, payload, callback);              
           break;
@@ -1281,6 +1284,27 @@ const funListBusinessSites = function (BASE_URL, payload, callback) {
   }
 
   let url = `${BASE_URL}ezpaySites/listSites`;
+  axios.post(url, payload).then(response => {
+    return callback(response);
+  }).catch((error) => {
+    let json = stringify(error);
+    return callback(json);
+  });
+}
+
+const funListSiteCredentials = function (BASE_URL, payload, callback) {
+
+  let businessId = payload["meta"]["businessId"];
+  if (isNull(businessId)) {
+    return callback(new HttpErrors.BadRequest('businessId is mandatory.', { expose: false }));
+  }
+
+  let locationId = payload["meta"]["locationId"];
+  if (isNull(locationId)) {
+    return callback(new HttpErrors.BadRequest('locationId is mandatory.', { expose: false }));
+  }
+
+  let url = `${BASE_URL}ezpaySites/listSiteCredentials`;
   axios.post(url, payload).then(response => {
     return callback(response);
   }).catch((error) => {
